3a3ed2f8dee3973abf52b889675a719cfe7fc38b
Per Jensen <arenatofte@gmail.com>
2021-02-17 12:21:20 +0000

    hopefully done

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_120642) do
 
   create_table "reviews", force: :cascade do |t|
     t.text "content"
+    t.integer "rating"
     t.integer "restaurant_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
