38e86012039f839b9d664b3b42263903092828c3
Per Jensen <arenatofte@gmail.com>
2021-02-17 12:22:45 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :restaurants, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:create]
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
   end
 end
